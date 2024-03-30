f79e388bf2d69c892644e2b866e96a04acac0951
Sameer Ahmad <me.sam990@gmail.com>
2024-03-30 06:46:18 +0530

    dev checkpoint 3

--- control-managers/src/client-manager-daemon/main.rs
+++ control-managers/src/client-manager-daemon/main.rs
@@ -3,21 +3,34 @@ mod vcuda_client_handler;
 #[path = "../common/mod.rs"]
 mod common;
 
-use std::{os::unix::net::{UnixListener, UnixStream}, sync::{Arc, Mutex}};
-use std::net::TcpStream;
-use std::io::{self, BufReader, Read, Write};
-use std::io::prelude::*;
-use std::fmt::{self, Display};
-use std::fs::File;
+use std::{fs::File, os::unix::net::UnixListener};
+use std::io::{Read, Write};
 use std::thread;
 use toml::Table;
 use vcuda_client_handler::VCudaClientManager;
 
-use crate::resource_manager_handler::{ ResourceManagerHandler, VirtServer };
+use crate::resource_manager_handler::ResourceManagerHandler;
 
 const SOCKET_PATH: &str = "/home/sam/Projects/flyt/control-managers/client-mgr.sock";
 const CONFIG_PATH: &str = "/home/sam/Projects/flyt/control-managers/client-mgr.toml";
 
+fn get_vcuda_process_monitor_period() -> u64 {
+    let mut file = File::open(CONFIG_PATH).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    let config = contents.parse::<Table>().unwrap();
+
+    let period = || -> Option<u64> {
+        Some(config.get("vcuda_client")?.get("process_monitor_period")?.as_integer()? as u64)
+    };
+
+    match period() {
+        Some(p) => p as u64,
+        None => 60u64
+    }
+}
 
 fn get_resource_mgr_address() -> (String, u16) {
     let mut file = File::open(CONFIG_PATH).unwrap();
@@ -60,17 +73,20 @@ fn main() {
 
     let (resource_manager_address, resource_manager_port) = get_resource_mgr_address();
 
-    let mut res_mgr = ResourceManagerHandler::new(resource_manager_address, resource_manager_port);
     let client_mgr = VCudaClientManager::new();
-
+    let res_mgr = ResourceManagerHandler::new(resource_manager_address, resource_manager_port, &client_mgr);
 
     thread::scope(|s| {
         s.spawn(|| {
-            //
+            let period = get_vcuda_process_monitor_period();
+            loop {
+                thread::sleep(std::time::Duration::from_secs(period));
+                client_mgr.remove_closed_clients(|| res_mgr.notify_zero_clients() );
+            }
         });
 
         s.spawn(|| {
-            incoming_vcuda_client_handler(&mut res_mgr, &client_mgr, s);
+            incoming_vcuda_client_handler(&res_mgr, &client_mgr, s);
         });
     });
 
