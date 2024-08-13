534adefd9243eaff48ab657fde7154f03870db83
armaan-chowfin <armaanchowfin@gmail.com>
2024-08-13 13:15:58 +0000

    updated clmgr config path in flyt-vm

--- control-managers/src/common/config.rs
+++ control-managers/src/common/config.rs
@@ -1,4 +1,4 @@
 
-pub const CLMGR_CONFIG_PATH: &str = "/home/armaan/Work/SYNERG/Puru_Flyt/flyt_src/flyt/configs/client-mgr.toml";
+pub const CLMGR_CONFIG_PATH: &str = "/home/vm/flyt_src/flyt/configs/client-mgr.toml";
 pub const RMGR_CONFIG_PATH: &str = "/home/armaan/Work/SYNERG/Puru_Flyt/flyt_src/flyt/configs/cluster-mgr-config.toml";
 pub const SNODE_CONFIG_PATH: &str = "/home/armaan/Work/SYNERG/Puru_Flyt/flyt_src/flyt/configs/servnode-config.toml";
