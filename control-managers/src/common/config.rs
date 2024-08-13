0cbdd14d6baf894500c34cff19acbbc51e8bfd14
pramodrao <pramodrao@cse.iitb.ac.in>
2024-08-13 19:09:42 +0530

    node manager configs setup. TODO: inspect >>unable to get GPU resources

--- control-managers/src/common/config.rs
+++ control-managers/src/common/config.rs
@@ -1,4 +1,4 @@
 
 pub const CLMGR_CONFIG_PATH: &str = "/home/vm/flyt_src/flyt/configs/client-mgr.toml";
 pub const RMGR_CONFIG_PATH: &str = "/home/armaan/Work/SYNERG/Puru_Flyt/flyt_src/flyt/configs/cluster-mgr-config.toml";
-pub const SNODE_CONFIG_PATH: &str = "/home/armaan/Work/SYNERG/Puru_Flyt/flyt_src/flyt/configs/servnode-config.toml";
+pub const SNODE_CONFIG_PATH: &str = "/home/ub-11/Armaan_Puru_Flyt/flyt_src/flyt/configs/servnode-config.toml";
